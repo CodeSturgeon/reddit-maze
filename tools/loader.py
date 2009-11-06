491d011c3de5c175ad234d7c9fe712b78f30e92f
Nick Fisher <github@nickdafish.com>
2009-11-06 01:10:35 -0500

    tools for external maze generation and import

---
+++ tools/loader.py
@@ -0,0 +1,22 @@
+from google.appengine.ext import db
+from google.appengine.tools import bulkloader
+import pickle
+import simplejson as json
+
+# Not sure why I need to do this... but model won't be found if I don't
+import os
+import sys
+sys.path.insert(0,os.curdir)
+
+from model import Tile
+
+class TileLoader(bulkloader.Loader):
+  def __init__(self):
+    bulkloader.Loader.__init__(self, 'Tile',
+                        [('x', int), ('y', int),
+                         ('view_blob', lambda v: pickle.dumps(
+                                    json.loads(v.replace('%','"'
+                                                ).replace('@',','))))
+                        ])
+
+loaders = [TileLoader]
