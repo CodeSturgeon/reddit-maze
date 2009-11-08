a38cdb44c3b2542e2517c9a11a0d1288f6ddf5f6
Nick Fisher <github@nickdafish.com>
2009-11-08 18:23:40 -0500

    /drop to clean datastore

---
+++ dropper.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+#
+# Copyright 2007 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+
+
+
+import wsgiref.handlers
+from model import Tile
+from google.appengine.ext import db
+from model import Avatar, Tile
+
+from google.appengine.ext import webapp
+
+
+class DroppedHandler(webapp.RequestHandler):
+
+  def get(self):
+    db.delete(Tile.all())
+    db.delete(Avatar.all())
+    self.response.headers['Content-type'] = 'text/plain'
+    self.response.out.write('Dropped!')
+
+def main():
+  application = webapp.WSGIApplication([('/drop', DroppedHandler)],
+                                       debug=True)
+  wsgiref.handlers.CGIHandler().run(application)
+
+
+if __name__ == '__main__':
+  main()
