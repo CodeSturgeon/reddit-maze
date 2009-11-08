a85d765cfb15647b16d9ffc1dabdec240adad484
Nick Fisher <github@nickdafish.com>
2009-11-08 13:21:30 -0500

    Comments and tweaks for generator

--- tools/maze_gen.py
+++ tools/maze_gen.py
@@ -2,6 +2,7 @@
 import random
 from optparse import OptionParser
 import simplejson as json
+import sys
 
 def dump_maze(width, height, cleared):
     print '#'*(width+2)
@@ -21,7 +22,7 @@ def dump_maze(width, height, cleared):
         print ''.join(tiles)
     print '#'*(width+2)
 
-def make_maze(width,height):
+def make_maze(width,height,updates):
     # Four basic direction vectors
     vectors = [(1,0),(0,1),(-1,0),(0,-1)]
     # Backtracking offset
@@ -35,7 +36,9 @@ def make_maze(width,height):
     # Setup tracking lists
     cleared=[]
     blocked=[]
+    count = 0
     while 1:
+        # Clear the current location if needed
         if (x,y) not in cleared:
             cleared.append((x,y))
 
@@ -43,16 +46,22 @@ def make_maze(width,height):
         # Look in each direction
         open = []
         for (x_d, y_d) in vectors:
+
             # adj is the x,y of the adjacent tile in chosen direction
             adj = (x+x_d, y+y_d)
+
+            # See if we can ignore this adjacent tile
             if adj[0]<x_min or adj[0] >= x_max:
+                # Ignore adj tiles off the left or right
                 continue
             if adj[1]<y_min or adj[1] >= y_max:
+                # Ignore adj tiles off the top or bottom
                 continue
             if adj in cleared or adj in blocked:
                 # If the adjacent tile was already hit by an overlapping pass
                 continue
-            # Block the adjacent tile if two or more of it's neighbors is clear
+
+            # Block the adjacent tile if two or more neighbors are clear
             # Might want to tinker with this to stop 1x1 spurs
             adj_cleared = 0
             for (x_d2, y_d2) in vectors:
@@ -64,17 +73,28 @@ def make_maze(width,height):
                     break
             else:
                 open.append(adj)
-        
 
         # Pick a next move
         if open == []:
+            # No open tiles means we need to backtrack
             offset += 1
             if offset > len(cleared):
+                # If we have back tracked to the begining... we are done
                 break
+            # Set the new position to previous tile
             x,y=cleared[-offset]
         else:
+            # Open tiles means no back tracking
             offset = 0
             x,y=random.choice(open)
+
+        if updates:
+            # Keep the user informed
+            count += 1
+            if (count % 500) == 0:
+                print count
+    if updates:
+        print 'final',count
     return cleared
 
 def save(name, width, height, cleared):
@@ -90,23 +110,31 @@ def save(name, width, height, cleared):
         tile_enc = json.dumps(view).replace(',','@').replace('"','%')
         line = '%d,%d,%s'%(x,y,tile_enc)
         lines.append(line)
-    open('test2.csv','w').write('\n'.join(lines))
+    open('%s.csv'%name,'w').write('\n'.join(lines))
 
 def get_options():
     parser = OptionParser()
     parser.add_option('-x', '--width', type='int', default=40)
     parser.add_option('-y', '--height', type='int', default=20)
     parser.add_option('-n', '--dry-run', action='store_true')
+    parser.add_option('-d', '--dump', action='store_true')
     parser.add_option('-q', '--quiet', action='store_true')
     return parser.parse_args()
 
 def main():
     opts, args = get_options()
     for name in args:
-        cleared = make_maze(opts.width,opts.height)
+        if opts.quiet:
+            updates = False
+        else:
+            updates = True
+            print 'Making %s'%name
+        cleared = make_maze(opts.width,opts.height,updates)
+        if not opts.quiet:
+            print '\nDone making %s'%name
         if not opts.dry_run:
             save(name, opts.width, opts.height, cleared)
-        if not opts.quiet:
+        if opts.dump:
             print '--%s--'%name
             dump_maze(opts.width, opts.height, cleared)
 
