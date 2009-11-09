6991ee785566f85e240da8140da555f8cc216191
Nick Fisher <github@nickdafish.com>
2009-11-08 22:42:54 -0500

    First shot at importer... WAY too slow

---
+++ tools/ninja_importer.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+import Image
+import csv
+import simplejson as json
+import sys
+
+f_path = '/Users/fish/Desktop/maze4.png'
+img = Image.open(f_path)
+x_min = 65
+x_max = 845 # 844 is last tile
+y_min = 37
+y_max = 460 # 459 is last tile
+
+vx=vy=0
+cleared = []
+for px in range(x_min, x_max):
+    for py in range(y_min,y_max):
+        if img.getpixel((px,py))==(0,0,0):
+            cleared.append((px-x_min,py-y_min))
+
+total = len(cleared)
+print 'Total cleared: %s'%total
+
+maze_writer = csv.writer(open('ninja.csv','w'), delimiter=',',
+                            quotechar='"', quoting=csv.QUOTE_MINIMAL)
+view_radius = 3
+last = []
+line_no = 0
+
+for x,y in cleared:
+    view = []
+    for vy in range(y-view_radius,y+view_radius+1):
+        for vx in range(x-view_radius,x+view_radius+1):
+            if (vx,vy) in last:
+                view.append({'x':vx, 'y':vy})
+            elif (vx,vy) in cleared:
+                view.append({'x':vx, 'y':vy})
+    last = list(view)
+    tile_enc = json.dumps(view)
+    maze_writer.writerow([x,y,tile_enc])
+    line_no += 1
+    if (line_no % 50) == 0:
+        print line_no
+        sys.exit()
