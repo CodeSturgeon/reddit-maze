f5cfb30f00dabf3b35afd72fd1429dc2a919b96b
Nick Fisher <github@nickdafish.com>
2009-11-08 19:00:27 -0500

    implemented memcache for Tiles

--- avatar.py
+++ avatar.py
@@ -22,6 +22,7 @@ import wsgiref.handlers
 
 
 from google.appengine.ext import webapp
+from google.appengine.api import memcache
 
 from model import Tile, Avatar
 from google.appengine.ext import db
@@ -47,8 +48,13 @@ class MainHandler(webapp.RequestHandler):
         if a is None:
             a = Avatar(x=0,y=0,name=name)
             a.put()
-        t = db.GqlQuery('SELECT * FROM Tile WHERE x = :1 AND y = :2', a.x, a.y
-                        ).get()
+        t = memcache.get('%d-%d'%(a.x,a.y))
+        if t is None:
+            log.info('Tile cache miss')
+            t = db.GqlQuery('SELECT * FROM Tile WHERE x = :1 AND y = :2', a.x,
+                            a.y).get()
+        else:
+            log.info('Tile cache hit')
         ret = {'avatar':a, 'tiles':t}
         ret_json = json.dumps(ret,indent=2,default=custom_encode)
         self.response.headers['Content-type'] = 'text/plain'
@@ -66,13 +72,18 @@ class MainHandler(webapp.RequestHandler):
         a = db.GqlQuery('SELECT * FROM Avatar WHERE name = :1', name).get()
         nx = shape_vector[move][0] + a.x
         ny = shape_vector[move][1] + a.y
-        t = db.GqlQuery('SELECT * FROM Tile WHERE x = :1 AND y = :2', nx, ny
-                        ).get()
+        t = memcache.get('%d-%d'%(nx,ny))
         if t is None:
-            self.error(400)
-            self.response.out.write({'code':400, 'error':'No phasing!'})
-            return
-        log.error('t: %s'%t)
+            log.info('Tile cache miss')
+            t = db.GqlQuery('SELECT * FROM Tile WHERE x = :1 AND y = :2', nx,
+                            ny).get()
+            if t is None:
+                self.error(400)
+                self.response.out.write({'code':400, 'error':'No phasing!'})
+                return
+            memcache.set('%d-%d'%(t.x,t.y),t.serial())
+        else:
+            log.info('Tile cache hit')
         a.x = nx
         a.y = ny
         a.put()
