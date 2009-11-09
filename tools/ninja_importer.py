d06af093f99a79c0a7e95f41f7d8b1953ae2f5f1
Nick Fisher <github@nickdafish.com>
2009-11-08 22:47:52 -0500

    Using dict lookups... MUCH faster

--- tools/ninja_importer.py
+++ tools/ninja_importer.py
@@ -12,11 +12,11 @@ y_min = 37
 y_max = 460 # 459 is last tile
 
 vx=vy=0
-cleared = []
+cleared = {}
 for px in range(x_min, x_max):
     for py in range(y_min,y_max):
         if img.getpixel((px,py))==(0,0,0):
-            cleared.append((px-x_min,py-y_min))
+            cleared[(px-x_min,py-y_min)]=True
 
 total = len(cleared)
 print 'Total cleared: %s'%total
@@ -24,18 +24,18 @@ print 'Total cleared: %s'%total
 maze_writer = csv.writer(open('ninja.csv','w'), delimiter=',',
                             quotechar='"', quoting=csv.QUOTE_MINIMAL)
 view_radius = 3
-last = []
 line_no = 0
 
 for x,y in cleared:
     view = []
     for vy in range(y-view_radius,y+view_radius+1):
         for vx in range(x-view_radius,x+view_radius+1):
-            if (vx,vy) in last:
+            try:
+                test = cleared[(vx,vy)]
                 view.append({'x':vx, 'y':vy})
-            elif (vx,vy) in cleared:
-                view.append({'x':vx, 'y':vy})
-    last = list(view)
+            except KeyError:
+                pass
+
     tile_enc = json.dumps(view)
     maze_writer.writerow([x,y,tile_enc])
     line_no += 1
