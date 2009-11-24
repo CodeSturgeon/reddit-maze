9c5a86db35f57c922d6d6283c8b9b8baf702d9b6
Nick Fisher <github@nickdafish.com>
2009-11-24 03:33:27 -0500

    complete rebuild of memcache/store handling

--- avatar.py
+++ avatar.py
@@ -56,17 +56,39 @@ def exceptable(meth):
             self.err(500,'Sorry... the data store timed out')
     return decorated
 
+def get_tile(maze_name, x, y):
+    key_path = ('Maze', maze_name, 'Tile', '%d-%d'%(x,y))
+    key = db.Key.from_path(*key_path)
+    return get_entity(key)
+
+def get_avatar(name):
+    key_path = ('Avatar', name)
+    key = db.Key.from_path(*key_path)
+    return get_entity(key)
+
+def get_entity(key):
+    entity = memcache.get(str(key))
+    if entity is None:
+        entity = db.get(key)
+    if entity is not None:
+        memcache.set(str(key),entity)
+    return entity
+
+def set_entity(entity):
+    entity.put()
+    memcache.set(str(entity.key()), entity)
+
 class MainHandler(ExceptionHandler):
 
     def get(self, name):
-        a = db.get(db.Key.from_path('Avatar', name))
-        if a is None:
-            a = Avatar(x=0,y=0,key_name=name)
-            a.put()
-        t = memcache.get('%d-%d'%(a.x,a.y))
-        if t is None:
-            t = db.get(db.Key.from_path('Tile','%d-%d'%(a.x,a.y)))
-        ret = {'avatar':a, 'tiles':t}
+        avatar_skey = str(db.Key.from_path('Avatar', name))
+        avatar = memcache.get(avatar_skey)
+        if avatar is None:
+            avatar = Avatar.get_or_insert(name)
+            memcache.set(avatar_skey, avatar)
+        maze_name = 'bcn'
+        base_tile = get_tile('bcn', avatar.x, avatar.y)
+        ret = {'avatar':avatar, 'tiles':base_tile}
         ret_json = json.dumps(ret,indent=2,default=custom_encode)
         self.response.headers['Content-type'] = 'application/json'
         self.response.out.write(ret_json)
@@ -75,9 +97,8 @@ class MainHandler(ExceptionHandler):
     def post(self, name):
         req_body = json.loads(self.request.body)
         moves = req_body['moves']
-        avatar = db.get(db.Key.from_path('Avatar', name))
-        pre_tile = db.get(db.Key.from_path('Tile','%d-%d'%(
-                                                        avatar.x,avatar.y)))
+        avatar = get_avatar(name)
+        pre_tile = get_tile('bcn', avatar.x, avatar.y)
         ret_tiles = {}
         for move in moves:
             # Get the move direction
@@ -101,21 +122,15 @@ class MainHandler(ExceptionHandler):
                 raise HTTPBadRequest(err_str)
             new_x = shape_vector[move_shape][0] + avatar.x
             new_y = shape_vector[move_shape][1] + avatar.y
-            tiles = memcache.get('%d-%d'%(new_x,new_y))
-            if tiles is None:
-                tile = db.get(db.Key.from_path('Tile','%d-%d'%(new_x,new_y)))
-                if tile is None:
-                    self.error(400)
-                    self.response.out.write(
-                                        {'code':400, 'error':'No phasing!'})
-                    return
-                tiles = tile.serial()
-                memcache.set('%d-%d'%(new_x,new_y),tiles)
+            tile = get_tile('bcn', new_x, new_y)
+            if tile is None:
+                err_str = 'Cannot move into wall (move: %d)'%move_lock
+                raise HTTPBadRequest(err_str)
             avatar.x = new_x
             avatar.y = new_y
             avatar.moves += 1
             if not seen:
-                for dt in tiles:
+                for dt in tile.serial():
                     ret_tiles[(dt['x'],dt['y'])] = dt['shape']
 
         for t in pre_tile.serial():
@@ -124,7 +139,7 @@ class MainHandler(ExceptionHandler):
         ret = {'avatar':avatar}
         ret['tiles'] = [{'x':t[0],'y':t[1],'shape':ret_tiles[t]}
                                                             for t in ret_tiles]
-        avatar.put()
+        set_entity(avatar)
         ret_json = json.dumps(ret,indent=2,default=custom_encode)
         self.response.headers['Content-type'] = 'application/json'
         self.response.out.write(ret_json)
